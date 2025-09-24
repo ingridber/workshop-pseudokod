69e64fe9291007263cc8a798b90403f910fdda0c
rut.ingrid.berggren@gmail.com <rut.ingrid.berggren@gmail.com>
2025-09-24 10:18:42 +0200

    Pseudocode Paying for the wall

---
+++ script.js
@@ -0,0 +1,19 @@
+// WHO IS PAYING FOR THE WALL
+    // Driver: Ingrid Navigator: Axel, Olivia
+
+// MÅL
+    // Korta ner namnet till max 2 bokstäver
+
+// INPUTS
+    // String - Namnet på grannarna
+
+// RETURN
+    // En lista med originalnamnet och förkortningen av namnet. ex ['Ingrid', 'In']
+
+// FUNKTIONSBESKRIVNING
+    /*
+    Kolla om namnet är över 2 bokstäver
+        OM TRUE: return [Fullname, truncated name (2 characters)]
+        OM FALSE: return the name as it is [Fullname]
+    */
+
